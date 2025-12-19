e160a6c8bc16e00067ee0ccc18efd58221d806d2
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2025-12-19 15:10:23 +0530

    make changes

---
+++ Backend/DAY07/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+
+const BookStore = [
+    {id:1,name:"Harry Potter", author:"DevFlux"},
+    {id:2, name:"Friends", author: "Vikas"},
+    {id:3 , name:"Nexus", author:"Rohit"},
+    {id:4 , name:"DSA", author:"Maharaj"},
+    {id:5, name:"Prem Kahani", author:"Rohan"}
+]
+
+app.use(express.json());
+
+// localhost:3000/book/3
+
+app.get("/book", (req,res)=>{
+
+    res.send(BookStore);
+})
+
+app.get("/book/:id", (req,res)=>{
+
+    const id = parseInt(req.params.id);
+    // console.log(typeof req.params.id)
+    const Book =  BookStore.find(info=> info.id===id);
+    res.send(Book); 
+})
+
+app.post("/book", (req,res)=>{
+    console.log(req.body);
+    BookStore.push(req.body);
+    res.send("Data Saved Successfully");
+})
+
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Listening at port 3000");
+})
+
+ 
