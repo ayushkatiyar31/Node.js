f4997ab8c4ad7bbc1dacec9e948984a0266ba0f5
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2025-12-27 02:39:37 +0530

    make changes

---
+++ Backend/DAY15/database.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+async function main() {
+
+ await mongoose.connect("mongodb+srv://ayushkatiyar:Ayush%40123@codingadda.y7en9oe.mongodb.net/BookStore");
+
+const userSchema = new Schema({
+  name: String,
+  age: Number,
+  city: String,
+  gender: String
+
+})
+
+// Create a Model === Create a collection(Create a table)
+
+const User =  mongoose.model("User", userSchema);
+
+// Create a Document === Create a object
+
+const user1 = new User({name:"Ayush", age:21, city:"Lucknow",gender:"Male"});
+await user1.save();
+
+
+}
+main()
+.then(() => console.log("Connected to DB"))
+.catch((err) => console.log("Error"));
